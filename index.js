401a39c843d14db53def9fea95e8c82daafe848a
Ammar <ammaraslam7164@gmail.com>
2023-07-15 01:03:51 +0600

    minimal-structure-done

--- index.js
+++ index.js
@@ -1,67 +1,40 @@
+const loadData = () => {
+  fetch("https://openapi.programming-hero.com/api/ai/tools")
+    .then((res) => res.json())
 
+    .then((data) => displayUsers(data.data.tools));
+};
 
+const displayUsers = (users) => {
+  console.log(users);
 
+  // step-1:  set container
 
-const loadData = () =>
+  const container = document.getElementById("card-container");
 
-{
+  // display 6 cards
 
-    fetch('https://openapi.programming-hero.com/api/ai/tools')
-     .then(res => res.json())
-     
-     .then( data =>
+  users = users.slice(0, 6);
 
- displayUsers(data.data.tools)
+  users.forEach((user) => {
+    console.log(user);
 
-     )}
+    // step-2: set div for every children
 
+    const cardDiv = document.createElement("div");
 
+    cardDiv.classList.add("col");
 
-
-const displayUsers=users=>{
-
-           
-    // console.log(users)
-
-
-    // step-1:  set container 
-          
-           const container= document.getElementById('card-container')
-
-          
-    
-
-
-
-       users.forEach(user=>{
-
-
-         console.log(user)
-                      
-     
-
-      
-
-
-        
-      
-        // step-2: set div for every children
-                
-        const  cardDiv = document.createElement('div');
-
-        cardDiv.classList.add('col')
-            
-                
-          cardDiv.innerHTML = `
+    cardDiv.innerHTML = `
           
           
           <div class="card h-100">
-          <img src="${user.image}" class="card-img-top "  alt="...">
-          <div class="card-body">
+          <img src="${user.image}" class="card-img-top img-fluid "  alt="...">
+          <div class="card-body ">
         
            
            
-          <h6 class="fw-bold fs-4 mt-3">Features: 
+          <h6 class="fw-bold fs-4 mt-3">Features
 
            <ol id="ol"> </ol>
              
@@ -86,33 +59,16 @@ const displayUsers=users=>{
           
 
           
-          `
-            
+          `;
 
-            // let  allFeatures = `<li>${user.features} </li>`;
-           
-               
-           
-            // document.querySelector('ol').insertAdjacentHTML('beforeend', allFeatures);
-           
+    // let  allFeatures = `<li>${user.features} </li>`;
 
+    // document.querySelector('ol').insertAdjacentHTML('beforeend', allFeatures);
 
+    // step-4: appendchild with container(parent)
 
+    container.appendChild(cardDiv);
+  });
+};
 
-            // step-4: appendchild with container(parent) 
-
-            container.appendChild(cardDiv);
-
-
-    })
-
-
-
-
-
-
-}
-
-
-
-loadData()
+loadData();
