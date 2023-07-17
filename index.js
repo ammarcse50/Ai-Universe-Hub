49dc9dc01a4b3260ca171d13098a3d38d4b21bfe
Ammar <ammaraslam7164@gmail.com>
2023-07-17 11:55:42 +0600

    looping features list done

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const loadData = () => {
   fetch("https://openapi.programming-hero.com/api/ai/tools")
     .then((res) => res.json())
 
-    .then((data) => displayUsers(data.data.tools));
+    .then((data) => displayUsers(data.data.tools))
+    .catch((error) => {
+      console.log(error);
+    });
 };
 
 const displayUsers = (users) => {
@@ -12,12 +15,63 @@ const displayUsers = (users) => {
 
   const container = document.getElementById("card-container");
 
-  // display 6 cards
+  // no card found
 
-  users = users.slice(0, 6);
+  // const  noFound = document.getElementById('no-card-found');
+
+  // if(users.length === 0)
+  // {
+  //   noFound.classList.remove('d-none')
+  // }
+  // else{
+  //   noFound.classList.add('d-none')
+  // }
+
+  // display 6 cards
+  // show all button here implemented
+
+
+  const showAll = document.getElementById("show-all");
+
+  if (users.length > 6) {
+
+    users = users.slice(0, 6);
+
+  
+
+    showAll.classList.remove("d-none");
+  } else {
+
+    showAll.classList.add("d-none");
+  }
+
+
+
 
   users.forEach((user) => {
     console.log(user);
+
+
+
+    //  extract make a list of  features array
+          
+               let feature = user.features;
+
+          function listOfItems(feature){
+            let items = "";
+
+            for(let i=0; i<user.features.length; i++)
+            {
+
+              items+= `<li>${user.features[i]} </li>`;
+              // console.log(items)
+
+
+
+            }
+            return items;
+          }
+         
 
     // step-2: set div for every children
 
@@ -31,34 +85,23 @@ const displayUsers = (users) => {
           <div class="card h-100">
           <img src="${user.image}" class="card-img-top img-fluid "  alt="...">
           <div class="card-body ">
-        
-           
-           
-          <h6 class="fw-bold fs-4 mt-3">Features
+                  
+          <h6 class="mt-3">Features
 
-           <ol id="ol"> </ol>
-             
-       
-       
-       
-       </h6>
-           
-           
-           
+           <ol id="ol" class="mt-4">${listOfItems()} </ol>
+       </h6>              
            </div>
-          
+
             <div class="card-footer">
             
-            <h5 class="card-title fw-5">${user.name}</h5>
+            <h5 class="card-title">${user.name}</h5>
             <small id="demo">time</small>
           </div>
          
             </div>
         </div>
           
-          
 
-          
           `;
 
     // let  allFeatures = `<li>${user.features} </li>`;
@@ -71,4 +114,16 @@ const displayUsers = (users) => {
   });
 };
 
+
+
+// loadData();
+
+
+// not the best way to load show all
+
+
+
+
 loadData();
+
+
